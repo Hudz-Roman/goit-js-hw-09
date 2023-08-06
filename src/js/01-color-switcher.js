fd08a7839f4137077f6b3541d602394a1df05f61
Roman Hudz <hudzrom@gmail.com>
2023-08-06 17:05:12 +0300

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick(evt) {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  intervalId = setInterval(changeBgColor, 1000);
+}
+
+function onStopClick(evt) {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(intervalId);
+}
+
+function changeBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
