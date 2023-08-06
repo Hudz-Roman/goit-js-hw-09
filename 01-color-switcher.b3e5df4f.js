a63307108ecbd66c993b598c4ae7e7772524417f
Hudz-Roman <126950170+Hudz-Roman@users.noreply.github.com>
2023-08-06 18:32:12 +0000

    Deploying to gh-pages from @ Hudz-Roman/goit-js-hw-09@ec97f77aee98631b4c47f1fdbdcaac125095fd9d 🚀

--- 01-color-switcher.b3e5df4f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,intervalId=setInterval(a,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(intervalId)}))}();
-//# sourceMappingURL=01-color-switcher.b3e5df4f.js.map
